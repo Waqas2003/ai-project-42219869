fde52c8594871f5c4d7d6a7a0729c6a0c34c0a2b
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-09 21:25:38 +0500

    Add src\utils\api.js

---
+++ src\utils\api.js
@@ -0,0 +1,15 @@
+const API_URL = 'https://example.com/api';
+
+const api = {
+  async login(email, password) {
+    // API call to login
+  },
+  async register(name, email, password) {
+    // API call to register
+  },
+  async getTrips() {
+    // API call to get trips
+  },
+};
+
+export default api;
